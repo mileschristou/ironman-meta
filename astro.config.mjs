7413a5acea4e8ffb260b9a09e6ba29eb217529f5
Miles Christou <mileschristou@Miless-MacBook-Air.local>
2026-02-10 14:03:45 +0000

    Fix sitemap URL to use www subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ironmanmeta.com',
+  site: 'https://www.ironmanmeta.com',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
